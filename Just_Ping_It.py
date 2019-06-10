470d3b373c57deac323f92ef34fd61a7f449f6ba
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2019-06-10 12:28:52 +0530

    Python Program To Ping IP Address

---
+++ Just_Ping_It.py
@@ -0,0 +1,11 @@
+#This simple program pings any site or host you want
+#and prints whether the host is up or down
+
+import os
+host = input("Enter IP address or Host name :")
+ping = os.system("Ping "+str(host))
+
+if ping == 0:
+    print("The host is UP!!!")
+else:
+    print("The host is DOWN :( ")
