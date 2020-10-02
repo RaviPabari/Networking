bc73018dc63b15992556e04d39c2c30014c39d37
Dwarkesh bhayani <57214244+dbhayani01@users.noreply.github.com>
2020-10-02 17:09:31 +0530

    Update Just_Ping_It.py

--- Just_Ping_It.py
+++ Just_Ping_It.py
@@ -2,7 +2,7 @@
 #and prints whether the host is up or down
 
 import os
-host = input("Enter IP address or Host name :")
+host = input("Enter IP address or Host name :")# input
 ping = os.system("ping "+str(host))
 
 if ping == 0:
